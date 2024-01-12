e0805e630ba9cf169c354457cb0bd51cba24308e
AWLR <lehrelex@gmail.com>
2024-01-12 17:11:43 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+     <UserForm @add:user="addUser" />
+     <UserList :users="users" />
+  </div>
+ </template>
+ 
+ <script>
+ import UserForm from "./components/UserForm.vue";
+ import UserList from "./components/UserList.vue";
+ 
+ export default {
+  components: {
+     UserForm,
+     UserList,
+  },
+  data() {
+     return {
+       users: [],
+       nextId: 1,
+     };
+  },
+  methods: {
+     addUser(user) {
+       user.id = this.nextId++;
+       this.users.push(user);
+     },
+  },
+ };
+ </script>
---
+++ src/components/UserForm.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+       <h1>User Form</h1>
+       <form @submit.prevent="submitForm">
+         <div>
+           <label for="name">Name:</label>
+           <input type="text" id="name" v-model="form.name" required />
+         </div>
+         <div>
+           <label for="email">Email:</label>
+           <input type="email" id="email" v-model="form.email" required />
+         </div>
+         <div>
+           <label for="age">Age:</label>
+           <input type="number" id="age" v-model="form.age" required />
+         </div>
+         <button type="submit">Submit</button>
+       </form>
+    </div>
+   </template>
+   
+   <script>
+   export default {
+    data() {
+       return {
+         form: {
+           name: "",
+           email: "",
+           age: null,
+         },
+       };
+    },
+    methods: {
+       submitForm() {
+         this.$emit("add:user", this.form);
+         this.form.name = "";
+         this.form.email = "";
+         this.form.age = null;
+       },
+    },
+   };
+   </script>
